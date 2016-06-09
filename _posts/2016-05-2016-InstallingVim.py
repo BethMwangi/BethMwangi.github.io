511dfe7cd0548b6d1b6321130a35ac9c71449c35
Wanjiku Mwangi <wanjikumwangib@gmail.com>
2016-06-09 14:13:02 +0300

    Vim for python Developers

---
+++ _posts/2016-05-2016-InstallingVim.py
@@ -0,0 +1,28 @@
+
+####VIM for Python Developers
+ I have just been introduced to python and the only source I am using is code academy. So the online editor will not do much for me since I need to practice on how to write python programs locally. It has been a week into python and I have come across several IDE's one can use but I found vim to be interesting. Additionally, I realized I can ditch my mouse when coding(No wastage of time..Yaay!)After researching on it, I  realize it is a powerful tool to get started with Python. 
+ 
+ I am going to explain on how i installed it in my ubuntu.The tutorial is a simple step by step that a newbie can follow. So let's dive in..
+
+###Installation
+Open your terminal and type the following commands 
+![Alt text](https://github.com/BethMwangi/blog_post/blob/master/img/img1?raw=true)
+
+**sudo apt-get update** 
+
+**sudo apt-get install vim** 
+
+**sudo apt-get update** will update the package lists and get newest versions of packages and their dependencies. 
+
+##The screen preview when updating..
+![Alt text](https://github.com/BethMwangi/blog_post/blob/master/img/img2?raw=true "Updating the packages")
+
+After the update, type **sudo apt-get install vim**  to now download the Vim to your machine 
+##The screen preview when downloading vim
+![Alt text](https://github.com/BethMwangi/blog_post/blob/master/img/img3?raw=true "Downloading Vim")
+
+You have now installed Vim and you can write your first python program.
+
+Happy Coding :)
+
+
